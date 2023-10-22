023dc689dba481f62b882047f4719ce0b91b0c8d
antonkonsta <akonstan@charlotte.edu>
2023-10-22 09:50:19 -0400

    modified:   viewer_webapp.py

--- viewer_webapp.py
+++ viewer_webapp.py
@@ -7,6 +7,12 @@ import plotly.express as px
 
 app = Flask(__name__)
 talisman = Talisman(app)
+
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 # Initialize global variables
 payload_mass = None
 df = None
